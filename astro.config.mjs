9cfee366d1ecaf574aab96d9ce5d13054b220414
bohecola <bohecola@outlook.com>
2024-04-02 18:43:08 +0800

    feat: use preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-bohe.netlify.app"
+  site: "https://astro-bohe.netlify.app",
+  integrations: [preact()]
 });
